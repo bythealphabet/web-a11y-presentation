fd03f85531c85f27a8f213640179173b7bd1f600
Isaac Lucas <isaac.lucas@dotcontrol.com>
2024-01-30 17:13:00 -0400

    card styling

--- app/(presentations)/presentations/page.tsx
+++ app/(presentations)/presentations/page.tsx
@@ -2,11 +2,42 @@ import { Card, CardsProps } from "@/components/cards/cards";
 import styles from "./presentation.module.scss";
 import clsx from "clsx";
 
-const dummyData: CardsProps[] = [
+interface ITopics {
+  title: string;
+  content: string;
+  description: string;
+  link: {
+    href: string;
+    label: string;
+  };
+  image: {
+    url: string;
+    alt: string;
+  };
+}
+
+interface IPresentations {
+  kind: "slide" | "topic";
+  title: string;
+  description: string;
+  link: {
+    href: string;
+    label: string;
+  };
+  image: {
+    url: string;
+    alt: string;
+  };
+  topics: ITopics[];
+}
+
+const dummyData: IPresentations[] = [
   {
-    kind: "slide",
-    title: "Card 1",
-    description: "This is the description for card 1.",
+    kind: "topic",
+    title: "Introduction to web accessibility",
+    description: `
+        What is web accessibility, kind of disabilities, Understanding the web accessibility standards and guidelines.
+    `,
     link: {
       href: "/",
       label: "Read more",
@@ -15,33 +46,63 @@ const dummyData: CardsProps[] = [
       url: "https://images.pexels.com/photos/3913019/pexels-photo-3913019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       alt: "Card 1 Image",
     },
-  },
-  {
-    kind: "slide",
-    title: "Card 2",
-    description: "This is the description for card 2.",
-    link: {
-      href: "/",
-      label: "Read more",
-    },
-    image: {
-      url: "https://images.pexels.com/photos/3913019/pexels-photo-3913019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      alt: "Card 2 Image",
-    },
+    topics: [
+      {
+        title: "What is web accessibility",
+        description: "This is the description for card 1.",
+        content: "This is the description for card 1.",
+        link: {
+          href: "/",
+          label: "Read more",
+        },
+        image: {
+          url: "https://images.pexels.com/photos/3913019/pexels-photo-3913019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          alt: "Card 1 Image",
+        },
+      },
+      {
+        title: "Why web accessibility",
+        description: "This is the description for card 1.",
+        content: "This is the description for card 1.",
+        link: {
+          href: "/",
+          label: "Read more",
+        },
+        image: {
+          url: "https://images.pexels.com/photos/3913019/pexels-photo-3913019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          alt: "Card 1 Image",
+        },
+      },
+      {
+        title: "How to make your website accessible",
+        description: "This is the description for card 1.",
+        content: "This is the description for card 1.",
+        link: {
+          href: "/",
+          label: "Read more",
+        },
+        image: {
+          url: "https://images.pexels.com/photos/3913019/pexels-photo-3913019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          alt: "Card 1 Image",
+        },
+      },
+    ],
   },
 ];
 
 async function Presentations() {
   return (
     <main className={clsx("sub-grid", styles.root)}>
-      <h1>Presentations Topics</h1>
-      <ul>
-        {dummyData.map((card, index) => (
-          <li key={index}>
-            <Card {...card} />
-          </li>
-        ))}
-      </ul>
+      <section>
+        <h1>Presentations Topics</h1>
+        <ul role="list">
+          {dummyData.map((card, index) => (
+            <li key={index}>
+              <Card {...card} />
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
